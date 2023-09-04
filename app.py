c6d7e484cb5765167f154c3aea5617da1fbda952
Eduardo <eduardo.camara@icomp.ufam.edu.br>
2023-09-03 23:06:55 -0400

    estetico

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+"""
+Nao sei pq pede isso, mas ta ai o comentario kk 
+"""
 from flask import Flask, render_template, redirect, url_for
 
 app = Flask("Meu Cadu")
@@ -5,15 +8,18 @@ app = Flask("Meu Cadu")
 
 # Comenario
 @app.route("/")
-def hello():
+def home():
+    """Rota principal da aplicação"""
     return render_template("home.html")
 
 
 @app.route("/novo")
 def novo():
+    """Nova rota"""
     return "<h1>Eu sou o novo Carlos Eduardo<h1>"
 
 
 @app.route("/redireciona")
 def redireciona():
+    """Redireciona para a rota novo"""
     return redirect(url_for("novo"))
