c138658aef107039f6023b59b8eaa1be7af72621
Eduardo <eduardo.camara@icomp.ufam.edu.br>
2023-09-03 23:00:20 -0400

    update

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
-app = Flask(__name__)
+app = Flask("Meu Cadu")
 # Comenario
 @app.route('/')
 def hello():
-    return "Eu sou o Carlos Eduardo"
+    return render_template('home.html')
     
 @app.route('/novo')
 def novo():
     return "<h1>Eu sou o novo Carlos Eduardo<h1>"
+
+@app.route('/redireciona')
+def redireciona():
+    return redirect(url_for('novo'))
