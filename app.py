cd82b9557a8ffc738e9faefef47bfab37744106e
Eduardo <139073033+educcamara@users.noreply.github.com>
2023-09-04 22:22:38 +0000

    fix

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """
 Nao sei pq pede isso, mas ta ai o comentario kk 
 """
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask("Meu Cadu")
 
@@ -12,10 +12,9 @@ def home():
     """Rota principal da aplicação"""
     return render_template("home.html")
 
+
 # Teste teste
 @app.route("/novo")
 def novo():
     """Nova rota"""
     return "<h1>Eu sou o novo Carlos Eduardo<h1>"
-
-
