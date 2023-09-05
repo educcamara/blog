f5a63b596dbc405d63b8603c0f93704a39184ab6
Eduardo <139073033+educcamara@users.noreply.github.com>
2023-09-05 04:02:37 +0000

    aesthetic

--- app.py
+++ app.py
@@ -6,9 +6,19 @@ from flask import Flask, render_template
 app = Flask("Meu Cadu")
 
 # Banco de dados Mock
-posts = [
-    {"título": "Andre: a thread", "texto": "teste teste"},
-    {"título": "Minha segunda postagem", "texto": "segundo teste"},
+posts_list = [
+    {
+        "titulo": "Andre: a thread", 
+        "texto": "teste teste", 
+        "data": "01/01/2021"},
+    {
+        "titulo": "Jaide: o merda",
+        "texto": "segundo teste",
+        "data": "01/03/2022"},
+    {
+        "titulo": "Por que guilhermes tendem a serem mais gostosos",
+        "texto": "terceiro teste",
+        "data": "05/10/2023"}
 ]
 
 
@@ -24,3 +34,8 @@ def sobre():
     """Rota sobre"""
     return render_template("sobre.html")
 
+
+@app.route("/posts")
+def posts():
+    """Rota posts"""
+    return render_template("posts.html", posts=posts_list)
