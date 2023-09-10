b83de9d32d92eea0ba14b090b1037af850501d11
Eduardo <139073033+educcamara@users.noreply.github.com>
2023-09-10 04:03:17 +0000

    sketch of site

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 """ My Blog Site """
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, \
+    flash, url_for, session, abort
+from posts import blog_posts
 
 app = Flask(__name__)
+app.config['SECRET KEY'] = 'cadu'
+
 
 @app.route('/')
 def home():
@@ -13,3 +17,44 @@ def home():
 def about():
     """ About page """
     return render_template('about.html')
+
+
+@app.route('/blog')
+def blog():
+    """ Blog page """
+    posts = blog_posts[::-1]
+    return render_template('blog.html', posts=posts)
+
+
+@app.route('/blog/<int:post_id>')
+def blog_post(post_id):
+    """ Blog post page """
+    try:
+        post = blog_posts[post_id - 1]
+        return render_template('post.html', post=post)
+    except IndexError:
+        abort(404)
+#
+# Login & Logout
+#
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """ Login page """
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Senha ou usuário inválidos'
+        else:
+            session['logged_in'] = True
+            flash('Salve ADM!')
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    """ Logout page """
+    session.pop('logged_in')
+    flash('Até mais ADM!')
+    return redirect(url_for('home'))
---
+++ posts.py
@@ -0,0 +1,15 @@
+""" List of blog posts """
+blog_posts = [
+    {
+        'title': 'Postagem 1',
+        'text': ''
+    },
+    {
+        'title': 'Postagem 2',
+        'text': ''
+    },
+    {
+        'title': 'Postagem 3',
+        'text': ''
+    }
+]
